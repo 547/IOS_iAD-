6105b6acbe4be2f34ca7ddad9820f97993a0f191
547 <1093031562@qq.com>
2016-03-25 15:23:32 +0800

    iAD的使用==加苹果官方的广告的方法
    在自己的app里面植入苹果官方的广告的方法

---
+++ iADTest/iADTest/ViewController.h
@@ -0,0 +1,54 @@
+//
+//  ViewController.h
+//  iADTest
+//
+//  Created by mac on 16/3/25.
+//  Copyright © 2016年 Seven. All rights reserved.
+//
+
+
+/**
+ IAD是苹果推出的广告平台，它可以帮助开发者从应用程序中获取收入。
+ 
+ 1. 创建一个简单的View based application
+ 2. 选择项目文件，然后选择目标，然后选择框架并添加 iAd.framework。
+ 3. 更新 ViewController.h
+ */
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+/**
+ -(void)iADTest
+{
+    //创建一个广告条视图
+    ADBannerView *bannerView=[[ADBannerView alloc]initWithFrame:CGRectMake(0, 21, self.view.frame.size.width, 50)];
+    bannerView.delegate=self;//设置代理
+    bannerView.backgroundColor=[UIColor yellowColor];
+    [self.view addSubview:bannerView];
+    
+}
+#pragma mark===ADBannerViewDelegate
+//接受广告失败是调用的方法
+-(void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"error===%@",error);
+}
+//已经加载广告
+-(void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    NSLog(@"已经加载广告");
+}
+//将要加载广告
+-(void)bannerViewWillLoadAd:(ADBannerView *)banner
+{
+    NSLog(@"将要加载广告");
+}
+//查看广告结束，回到本页面调用的方法
+-(void)bannerViewActionDidFinish:(ADBannerView *)banner
+{
+    NSLog(@"bannerViewActionDidFinish");
+}
+ */
